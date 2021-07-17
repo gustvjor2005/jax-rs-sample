24cc0bc783dd4477bf3ba9e29e762c712bb15a29
gustavo <gustavo@gustavo-G3-3579>
2021-07-17 06:37:25 -0500

    agregado roma

--- Alfa/src/main/java/com/App.java
+++ Alfa/src/main/java/com/App.java
@@ -3,7 +3,7 @@ package com;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("greece")
+@ApplicationPath("roma")
 public class App extends Application {
 
 	
